52696875038951031667d94907fc34303c82a5dd
Jamal Uddin Khan <jamal919@gmail.com>
2018-04-05 21:57:02 +0200

    Updates descriptions

--- SCHISMMB.py
+++ SCHISMMB.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 SCHISM Model Builder (SCHISMMB)
 
-SCHISMMB is the container of the the classes and methods to build the the 
-inputs for SCHISM Modelling system. 
+SCHISMMB is the container of the the classes and methods to build the inputs 
+for SCHISM Modelling system. 
+
+The model input is first build using the following objects - 
+    Hgrid:  Horizontal grid
+    Vgrid:  Vertical grid
+    Bctide: Boundary conditions
+    
+These objects are passed to the Project object with a directory to create the 
+files to run the model.
 
 @author: Md. Jamal Uddin Khan
 @email: jamal.khan@legos.obs-mip.fr
@@ -12,7 +20,7 @@ inputs for SCHISM Modelling system.
 import os
 import numpy as np
 
-class project(object):
+class Project(object):
     """
     SCHISM model project object. It only contains Path information regarding
     the model project and passed to the model object for further processing.
@@ -39,10 +47,13 @@ class project(object):
         os.rmdir(self.path)
 
 
-class hgrid(object):
+class Hgrid(object):
     """
     SCHISM hgrid.gr3 object. This object loads the hgrid.gr3 file, breaks it in
-    it's subcomponent, and store it for the model object to use. 
+    it's subcomponent, and store it for the model object to use.
+    
+    Grid or *.gr3 is essentially the fort.14 file format in ADCIRC model in SMS. 
+    The intended way for this model is to provide the original file from SMS.
     """
     def __init__(self, path = None):
         self.path = path
@@ -52,9 +63,8 @@ class hgrid(object):
             print('Check the hgrid.gr3 file location')
         
     def read(self): 
-        """ Read hgrid.gr3 file """
+        """ Read hgrid file in gr3 format """
         print("Reading hgrid.gr3 file")
         
     def plot(self):
         """ Plot the hgrid.gr3 mesh """
-        print('Plotting hgrid.gr3 file')
