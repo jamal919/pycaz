218bfac6ac552231bc6e121a6a5ef3a537921bfa
Jamal Khan <jamal919@gmail.com>
2018-07-18 15:27:12 +0600

    wwm boundary create script

--- wwm_create_boundary.py
+++ wwm_create_boundary.py
@@ -7,7 +7,17 @@ Created on Sun Jul 15 15:04:20 2018
 
 from SCHISM import schismio
 grid = schismio.Gr3()
-grid.initfromfile(path='/run/media/khan/Workbench/Educations/SCHISM/Simulations/CycloneMora/Tide_Wind_Pressure/hgrid.gr3')
-nodes = grid.readnodes()
-elems= grid.readelems()
-openbnd, landbnd = grid.readbounds()
+grid.readfromfile(path='/run/media/khan/Workbench/Educations/SCHISM/Simulations/CycloneMora/Tide_Wind_Pressure/hgrid.gr3')
+
+# First setting all points to 0 (not on boundary)
+grid.dnodes[:, 3] = 0
+
+# All boundaries are exterior boundary (1)
+for boundary in grid.openbnd.boundaries:
+    grid.dnodes[[i - 1 for i in boundary.nodes], 3] = 1
+    
+for boundary in grid.landbnd.boundaries:
+    grid.dnodes[[i - 1 for i in boundary.nodes], 3] = 1
+
+# Writing the grid file
+grid.writetofile(path='./test.gr3', overwrite=True, nodevalfmt='%4i')
