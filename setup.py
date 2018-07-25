1289b6e5ea075d1c207f4a16f46e3317f5b8fd1c
Jamal Khan <jamal919@gmail.com>
2018-07-25 13:06:30 +0600

    setup.py for schism package

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 25 12:59:35 2018
+
+@author: khan
+"""
+
+from distutils.core import setup
+
+setup(
+    name='schism',
+    version='0.1',
+    packages=['schism',],
+    license='GPL v.3',
+    long_description=open('README.md').read(),
+)
