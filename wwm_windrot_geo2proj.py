dbe37f1ff02016068ebe5d1e433858da973d8918
Jamal Khan <jamal919@gmail.com>
2018-07-25 10:06:29 +0600

    sample windrot file creation script

---
+++ wwm_windrot_geo2proj.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 18 16:55:28 2018
+
+@author: khan
+"""
+
+from SCHISM import schismio
+grid = schismio.Gr3()
+grid.readfromfile(path='/run/media/khan/Workbench/Educations/SCHISM/Simulations/CycloneMora/Tide_Wind_Pressure/hgrid.gr3')
+
+# First setting all points to 0 (not on boundary)
+grid.dnodes[:, 3] = 0
+
+# Writing the grid file
+grid.writetofile(path='./windrot_geo2proj.gr3', overwrite=True, writebounds=False, nodevalfmt='%4i')
