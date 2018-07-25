0258c3b484496ed5aa476822cfe0fc4db8725f90
Jamal Khan <jamal919@gmail.com>
2018-07-25 12:25:57 +0600

    update sample scripts

--- wwm_create_boundary.py
+++ wwm_create_boundary.py
@@ -5,8 +5,8 @@ Created on Sun Jul 15 15:04:20 2018
 @author: khan
 """
 
-from SCHISM import schismio
-grid = schismio.Gr3()
+from schism import io
+grid = io.Gr3()
 grid.readfromfile(path='/run/media/khan/Workbench/Educations/SCHISM/Simulations/CycloneMora/Tide_Wind_Pressure/hgrid.gr3')
 
 # First setting all points to 0 (not on boundary)
--- wwm_windrot_geo2proj.py
+++ wwm_windrot_geo2proj.py
@@ -5,8 +5,8 @@ Created on Wed Jul 18 16:55:28 2018
 @author: khan
 """
 
-from SCHISM import schismio
-grid = schismio.Gr3()
+from schism import io
+grid = io.Gr3()
 grid.readfromfile(path='/run/media/khan/Workbench/Educations/SCHISM/Simulations/CycloneMora/Tide_Wind_Pressure/hgrid.gr3')
 
 # First setting all points to 0 (not on boundary)
