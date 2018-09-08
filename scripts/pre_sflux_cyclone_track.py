f01ba165392c887db23efa9f21bfbd96e459a103
Jamal Khan <jamal919@gmail.com>
2018-09-08 18:36:52 +0600

    added sflux script

---
+++ scripts/pre_sflux_cyclone_track.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Create wind and pressure field from cyclone track and write it into SCHISM
+complient sflux file. 
+
+@author: khan
+@email: jamal.khan@legos.obs-mip.fr
+"""
