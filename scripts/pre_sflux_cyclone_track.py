d3d2dab2091ad71221f74b0ccf509e641c190e5e
Jamal Khan <jamal919@gmail.com>
2018-09-08 23:30:36 +0600

    sflux skeleton added

--- scripts/pre_sflux_cyclone_track.py
+++ scripts/pre_sflux_cyclone_track.py
@@ -6,3 +6,37 @@ complient sflux file.
 @author: khan
 @email: jamal.khan@legos.obs-mip.fr
 """
+
+from netCDF4 import Dataset
+
+class Track(object):
+    def __init__(self, time, lon, lat, P=None, Vmax=None, Rmax=None):
+        pass
+
+class Grid(object):
+    def __init__(self, x, y):
+        pass
+
+class WindInterpolator(object):
+    def __init__(self, grid, method='E11'):
+        pass
+
+    def interpolate(self, at):
+        pass
+
+class PressureInterpolator(object):
+    def __init__(self, grid, method='H80'):
+        pass
+
+    def interpolate(self, at):
+        pass
+
+class Sflux(object):
+    def __init__(self, grid, inittime, nstep, path='./'):
+        pass
+
+    def __create_netcdf(self, filename):
+        pass
+    
+    def putvalue(self, uwind, vwind, prmsl, stmp, spfh):
+        pass
