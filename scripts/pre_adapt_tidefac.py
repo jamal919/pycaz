23de0dae22d325b08fcde16fb76e5b45b642d2df
Jamal Khan <jamal919@gmail.com>
2018-09-18 20:40:24 +0600

    update bctides

--- scripts/pre_adapt_tidefac.py
+++ scripts/pre_adapt_tidefac.py
@@ -8,6 +8,7 @@ outputs.
 """
 from __future__ import print_function
 import numpy as np
+from datetime import datetime, timedelta
 import re
 
 class Bctides(object):
@@ -18,7 +19,8 @@ class Bctides(object):
         with open(self.filepath) as f:
             ds = f.readlines()
             # First the dates
-            self.date = ds[0].split('\n')[0]
+            self.dateline = ds[0].split('\n')[0]
+            print(self.dateline)
             __lnproc = 0
 
             # Then the tidal potential information
@@ -54,12 +56,25 @@ class Bctides(object):
 
             # For each open boundary sagment
             self.boundaries = ds[__lnproc+1:len(ds)]
-            print(self.boundaries)
 
-    def update(self):
-        pass
+    def update(self, tidefac):
+        # Update time
+        self.dateline = print(tidefac)
+        # Updating the tidal potential nodal factor and equilibrium argument
+        for __tip in self.tip:
+            __talpha = __tip['talpha'].strip().upper()
+            if __talpha in tidefac.const.keys():
+                __tip['tnf'] = tidefac.const[__talpha][0]
+                __tip['tear'] = tidefac.const[__talpha][1]
 
-    def write(self):
+        # Updating the Boundary frequency nodal factors and equilibrium argument
+        for __bfr in self.bfr:
+            __alpha = __bfr['alpha'].strip().upper()
+            if __alpha in tidefac.const.keys():
+                __bfr['ff'] = tidefac.const[__alpha][0]
+                __bfr['face'] = tidefac.const[__alpha][1]
+
+    def write(self, filepath):
         pass
 
 class Tidefacout(object):
@@ -77,8 +92,7 @@ class Tidefacout(object):
             self.hour = __date[0]
             self.day = __date[1]
             self.month = __date[2]
-            self.year = __date[3]
-            
+            self.year = __date[3]            
             # Reading the run length section
             __ds = f.readline()
             __rnday = self.r.findall(__ds)
@@ -90,13 +104,15 @@ class Tidefacout(object):
             self.rnday = __rnday[0]
 
         # Reading the constants, node factor and eq. argument ref. to GM in deg.
-        self.const = np.genfromtxt(fname=self.filepath, dtype=None, skiprows=8, \
-                                delimiter=None, names=True, autostrip=True)
-        self.const = np.array([[i for i in j] for j in self.const])
-        print(self.const.shape)
+        __const = np.genfromtxt(fname=self.filepath, dtype=None, skiprows=8, \
+                                delimiter=None, autostrip=True)
+        __const = np.array([[i for i in j] for j in __const])
+        __const = {i[0].upper():[float(j) for j in i[1:3]] for i in __const}
+        self.const = __const
+
 
     def __str__(self):
-        __msg = 'Tidefac output for {:.2f} days run starting from {:4.0f}/{:02.0f}/{:02.0f} {:02.1f} UTC'.format(self.rnday,\
+        __msg = '{:.2f} days run starting from {:4.0f}/{:02.0f}/{:02.0f} {:02f} UTC'.format(self.rnday,\
                 self.year, self.month, self.day, self.hour)
         return(__msg)
 
@@ -104,6 +120,9 @@ if __name__=='__main__':
     # file = '/run/media/khan/Workbench/Projects/Surge Model/Benchmark_Model_Mesh/Test02/tide_fac.out'
     # tfout = Tidefacout(filepath=file)
     # print(tfout)
-    file = '/run/media/khan/Workbench/Projects/Surge Model/Benchmark_Model_Mesh/Test02/bctides.in'
-    bctides = Bctides(filepath=file)
+    bctidefile = '/run/media/khan/Workbench/Projects/Surge Model/Benchmark_Model_Mesh/Test01/bctides.ini'
+    bctides = Bctides(filepath=bctidefile)
     bctides.read()
+    tidefacfile = '/run/media/khan/Workbench/Projects/Surge Model/Benchmark_Model_Mesh/Test01/tide_fac.out'
+    tidefac = Tidefacout(filepath=tidefacfile)
+    bctides.update(tidefac)
