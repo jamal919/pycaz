bf16649d65a440ecc0e01468d7cb05ceab31b486
Jamal Khan <jamal919@gmail.com>
2018-11-28 12:27:33 +0100

    prepost library file

---
+++ schism/postprocess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+SCHISM result post processing
+
+TODO:
+    - merge maxelev
+    - merge max uv
+    - merge netcdf outputs
+    - unstructured to structured interpolation
+
+@author: khan
+@email: jamal.khan@legos.obs-mip.fr
+"""
---
+++ schism/preprocess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+SCHISM preprocessing library
+
+TODO:
+    - create bctides.in
+    - create hgrid based input files
+    - create flux files
+
+@author: khan
+@email: jamal.khan@legos.obs-mip.fr
+"""
