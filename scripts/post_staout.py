cb55beaaeb2e16cb9798178f82dc2f593be79e91
Jamal Khan <jamal919@gmail.com>
2018-12-05 11:52:05 +0100

    staout processing

---
+++ scripts/post_staout.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script reads a staout file, station.in file and generate named time series.
+It can write a timeseries to REFMAR to be used with comodo-tools, or can directly
+be used with utide.
+
+author: Md Jamal Uddin Khan
+email: jamal.khan@legos.obs-mip.fr
+"""
+
+from __future__ import print_function
+import os
