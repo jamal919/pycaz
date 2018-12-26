f78b66b89566f7f21c25847b08396b552c661065
Jamal Khan <jamal919@gmail.com>
2018-12-26 18:22:51 +0100

    boundary error removed

--- scripts/pre_adapt_tidefac.py
+++ scripts/pre_adapt_tidefac.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Reads tidefac output and adapt given bctides file as required by the tidefac 
@@ -11,11 +10,10 @@ from __future__ import print_function
 import numpy as np
 from datetime import datetime, timedelta
 import sys
-import os
 import re
 
 class Bctides(object):
-    def __init__(self, info='', ntip=0, tip_dp=0, tip={}, nbfr=0, bfr={}, nope=0, boundaries=[]):
+    def __init__(self, info='', ntip=0, tip_dp=0, tip=[], nbfr=0, bfr=[], nope=0, boundaries=[]):
         self.info = info
         self.nitp = ntip
         self.tip_dp = tip_dp
@@ -39,9 +37,9 @@ class Bctides(object):
             
             for i in np.arange(self.ntip):
                 __talpha = ds[__lnproc+1].split('\n')[0]
-                print('Reading Wave {:d} - {:s}'.format(i, __talpha))
                 __jspc, __tamp, __tfreq, __tnf, __tear = np.fromstring(ds[__lnproc+2].split('\n')[0], count=5, sep=' ')
-                self.tip[__talpha.strip().upper()] = dict(jspc=int(__jspc), tamp=__tamp, tfreq=__tfreq, tnf=__tnf, tear=__tear)
+                __rec = dict(talpha=__talpha, jspc=__jspc, tamp=__tamp, tfreq=__tfreq, tnf=__tnf, tear=__tear)
+                self.tip.append(__rec)
                 __lnproc = __lnproc + 2
             
             # Reading the boundary frequencies
@@ -49,10 +47,12 @@ class Bctides(object):
             self.nbfr = int(self.nbfr)
             __lnproc = __lnproc + 1
             
+            self.bfr = []
             for i in np.arange(self.nbfr):
                 __alpha = ds[__lnproc+1].split('\n')[0]
                 __amig, __ff, __face = np.fromstring(ds[__lnproc+2].split('\n')[0], count=3, sep=' ')
-                self.bfr[__alpha.strip().upper()] = dict(alpha=__alpha, amig=__amig, ff=__ff, face=__face)
+                __rec = dict(alpha=__alpha, amig=__amig, ff=__ff, face=__face)
+                self.bfr.append(__rec)
                 __lnproc = __lnproc + 2
             
             # Open boundary sagments
@@ -67,16 +67,18 @@ class Bctides(object):
         # Update time
         self.info = tidefac.info
         # Updating the tidal potential nodal factor and equilibrium argument
-        for talpha in self.tip.keys():
-            if talpha in tidefac.const.keys():
-                self.tip[talpha]['tnf'] = tidefac.const[talpha][0]
-                self.tip[talpha]['tear'] = tidefac.const[talpha][1]
+        for __tip in self.tip:
+            __talpha = __tip['talpha'].strip().upper()
+            if __talpha in tidefac.const.keys():
+                __tip['tnf'] = tidefac.const[__talpha][0]
+                __tip['tear'] = tidefac.const[__talpha][1]
 
         # Updating the Boundary frequency nodal factors and equilibrium argument
-        for alpha in self.bfr.keys():
-            if alpha in tidefac.const.keys():
-                self.bfr[alpha]['ff'] = tidefac.const[alpha][0]
-                self.bfr[alpha]['face'] = tidefac.const[alpha][1]
+        for __bfr in self.bfr:
+            __alpha = __bfr['alpha'].strip().upper()
+            if __alpha in tidefac.const.keys():
+                __bfr['ff'] = tidefac.const[__alpha][0]
+                __bfr['face'] = tidefac.const[__alpha][1]
 
     def write(self, filepath):
         with open(filepath, 'w') as f:
@@ -86,24 +88,24 @@ class Bctides(object):
             # Tidal potential
             f.write('{:d} {:3.2f} !ntip, tip_dp\n'.format(int(self.ntip), float(self.tip_dp)))
 
-            for alpha in self.tip.keys():
+            for __tip in self.tip:
                 f.write('{:s}\n{:d}\t{:.6f}\t{:.16f}\t{:.5f}\t{:.2f}\n'\
-                        .format(alpha,\
-                                int(self.tip[alpha]['jspc']),\
-                                self.tip[alpha]['tamp'],\
-                                self.tip[alpha]['tfreq'],\
-                                self.tip[alpha]['tnf'],\
-                                self.tip[alpha]['tear']))
+                        .format(__tip['talpha'].strip().upper(),\
+                                int(__tip['jspc']),\
+                                __tip['tamp'],\
+                                __tip['tfreq'],\
+                                __tip['tnf'],\
+                                __tip['tear']))
 
             # Boundary frequencies
             f.write('{:d} !nbfr\n'.format(int(self.nbfr)))
 
-            for alpha in self.bfr.keys():
+            for __bfr in self.bfr:
                 f.write('{:s}\n{:.16E}\t{:.6f}\t{:.2f}\n'\
-                        .format(alpha,\
-                                self.bfr[alpha]['amig'],\
-                                self.bfr[alpha]['ff'],\
-                                self.bfr[alpha]['face']))
+                        .format(__bfr['alpha'].strip().upper(),\
+                                __bfr['amig'],\
+                                __bfr['ff'],\
+                                __bfr['face']))
 
             # Open boundaries
             f.write('{:d} !Number of Open Boundaries\n'.format(self.nope))
@@ -151,10 +153,9 @@ class Tidefacout(object):
         return(self.info)
 
 if __name__=='__main__':
-    path = '/run/media/khan/Workbench/Projects/Surge Model/Bctides'
-    bctide_source = os.path.join(path, 'bctides.ini')
-    bctide_update = os.path.join(path, 'bctides.in')
-    tfacfile = os.path.join(path, 'tide_fac.out')
+    bctide_source = 'bctides.ini'
+    bctide_update = 'bctides.in'
+    tfacfile = 'tide_fac.out'
     bctides = Bctides()
     bctides.read(filepath=bctide_source)
     tfac = Tidefacout()
