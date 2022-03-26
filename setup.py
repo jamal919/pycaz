d3e33935e23a4e0215205345b71d877f6ed3973e
Jamal Khan <jamal919@gmail.com>
2022-03-26 13:55:33 +0100

    :wrench: Updates environment and install script

--- setup.py
+++ setup.py
@@ -9,19 +9,28 @@ import setuptools
 
 setuptools.setup(
     name='pycaz',
-    version='0',
     author='Jamal Khan',
     author_email='4151009+jamal919@users.noreply.github.com',
     description='A python based analysis toolbox',
     packages=setuptools.find_packages(),
     license='GPL v3',
+    setup_requires=['setuptools-git-versioning', 'setuptools_scm'],
+    setuptools_git_versioning={
+        "enabled": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.post{ccount}+git.{sha}",
+        "dirty_template": "{tag}.post{ccount}+git.{sha}.dirty",
+        "starting_version": "0.1"
+    },
+    python_requires='>=3.6',
     install_requires=[
         'numpy',
         'scipy',
         'matplotlib',
-        'pandas'
+        'pandas',
+        'xarray'
     ],
     include_package_data=True,
     url="https://github.com/jamal919/pycaz",
-    long_description=open('README.md').read(),
+    long_description=open('README.md').read()
 )
