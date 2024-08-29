a98ed7e3334a7bffc5936464d1ae7bf29a505b02
Jamal Khan <jamal919@gmail.com>
2024-08-29 19:57:33 +0200

    :memo: Updates sflux module
    - Adds filename formatter
    - Adds nfile initial value
    - Adds documentation
    - Applies code formatting

--- pycaz/schism/sflux.py
+++ pycaz/schism/sflux.py
@@ -7,29 +7,74 @@ from datetime import datetime, timedelta
 from netCDF4 import Dataset
 import os
 
+
+def default_filename_formatter(sflux: Sflux) -> str:
+    """
+    Default name formatter for the Sflux file which follows the naming conventions from schism model.
+
+    :param sflux: Sflux object
+    :return: filename string
+    """
+    return f'sflux_{sflux.sflux_type}_{sflux.priority:1d}.{sflux.nfile:04d}.nc'
+
+
 class Sflux(object):
-    def __init__(self, grid, basedate, sflux_type='air', nstep=96, priority=1, syncstep=10, path='./sflux'):
+    def __init__(self,
+                 grid,
+                 basedate,
+                 sflux_type='air',
+                 nstep=96,
+                 priority=1,
+                 syncstep=10,
+                 nfile=0,
+                 filename_formatter=default_filename_formatter,
+                 path='./sflux'):
+        """
+        Generate SCHISM complient Sflux files
+
+        :param grid:
+        :param basedate:
+        :param sflux_type:
+        :param nstep:
+        :param priority:
+        :param syncstep:
+        :param path:
+
+        TODO:
+            - Validate inputs
+            - Update to a context interface
+            - Separate into Sub-classes
+            - Automate dt of the dataset
+            - Allow numpy datetime64 objects
+            - Separate netcdf file creation class, with data access, close function
+        """
         self.grid = grid
-        self.nstep = nstep # No of step
+        self.nstep = nstep  # No of step
         self.basedate = basedate
         self.sflux_type = sflux_type
-        self.nfile = 0 # No file at the beginning
-        self.priority = priority # sflux_air_1 or 2
-        self.syncstep = syncstep # Sync the netCDF each syncstep
+        self.nfile = nfile  # No file at the beginning
+        self.priority = priority  # sflux_air_1 or 2
+        self.syncstep = syncstep  # Sync the netCDF each syncstep
+        self.filename_formatter = filename_formatter
         self.path = path
 
+        self.nc = None
+        self.filepath = None
+        self.filename = None
+        self.step = None
+
         sflux_func_map = {
-            'air' : {
-                'create_netcdf' : self.create_netcdf_air,
-                'put_value' : self.put_value_air
+            'air': {
+                'create_netcdf': self.create_netcdf_air,
+                'put_value': self.put_value_air
             },
-            'prc' : {
-                'create_netcdf' : self.create_netcdf_prc,
-                'put_value' : self.put_value_prc
+            'prc': {
+                'create_netcdf': self.create_netcdf_prc,
+                'put_value': self.put_value_prc
             },
-            'rad' : {
-                'create_netcdf' : self.create_netcdf_rad,
-                'put_value' : self.put_value_rad
+            'rad': {
+                'create_netcdf': self.create_netcdf_rad,
+                'put_value': self.put_value_rad
             }
         }
 
@@ -38,7 +83,7 @@ class Sflux(object):
             self.put_value = sflux_func_map[sflux_type]['put_value']
         else:
             raise Exception(f"sflux_type {self.sflux_type} not correct, one of 'air', 'prc', and 'rad'")
-        
+
         # Directory creation
         if not os.path.exists(self.path):
             os.mkdir(self.path)
@@ -46,17 +91,16 @@ class Sflux(object):
     def create_netcdf_air(self):
         self.step = 0
         self.nfile = self.nfile + 1
-        self.filename = f'sflux_air_{self.priority:1d}.{self.nfile:04d}.nc'
+        self.filename = self.filename_formatter(self)
         self.filepath = os.path.join(self.path, self.filename)
 
         # Creating the file first
         self.nc = Dataset(self.filepath, 'w', format='NETCDF4_CLASSIC')
 
-        
         # Creating the dimensions
         self.nc.createDimension(dimname='nx_grid', size=len(self.grid.x))
         self.nc.createDimension(dimname='ny_grid', size=len(self.grid.y))
-        self.nc.createDimension(dimname='time', size=None)
+        self.nc.createDimension(dimname='time', size=None) # unlimited
 
         # Creating the variables
         # Time
@@ -140,13 +184,12 @@ class Sflux(object):
         self.v_spfh.units = '1'
         self.v_spfh.long_name = 'Specific Humidity (2m AGL)'
         self.v_spfh.standard_name = 'surface_specific_humidity'
-        
+
         # Writing lon-lat once
         X, Y = self.grid.meshgrid
         self.v_lon[:] = X.T
         self.v_lat[:] = Y.T
-        
-    
+
     def put_value_air(self, stepi, at, flux):
         if isinstance(at, (datetime, pd.DatetimeIndex)):
             at = pd.to_datetime(at) - self.basedate
@@ -155,7 +198,7 @@ class Sflux(object):
         else:
             raise Exception(f'at must be datetime or timedelta object')
 
-        self.v_time[stepi] = at.days + at.seconds/float(86400)
+        self.v_time[stepi] = at.days + at.seconds / float(86400)
         self.v_uwind[stepi, :, :] = flux['uwind']
         self.v_vwind[stepi, :, :] = flux['vwind']
         self.v_prmsl[stepi, :, :] = flux['prmsl']
@@ -169,17 +212,16 @@ class Sflux(object):
     def create_netcdf_prc(self):
         self.step = 0
         self.nfile = self.nfile + 1
-        self.filename = f'sflux_prc_{self.priority:1d}.{self.nfile:04d}.nc'
+        self.filename = self.filename_formatter(self)
         self.filepath = os.path.join(self.path, self.filename)
 
         # Creating the file first
         self.nc = Dataset(self.filepath, 'w', format='NETCDF4_CLASSIC')
 
-        
         # Creating the dimensions
         self.nc.createDimension(dimname='nx_grid', size=len(self.grid.x))
         self.nc.createDimension(dimname='ny_grid', size=len(self.grid.y))
-        self.nc.createDimension(dimname='time', size=None)
+        self.nc.createDimension(dimname='time', size=None) # unlimited
 
         # Creating the variables
         # Time
@@ -223,7 +265,7 @@ class Sflux(object):
         self.v_prate.units = 'kg/m^2/s'
         self.v_prate.long_name = 'Surface Precipitation Rate'
         self.v_prate.standard_name = 'precipitation_flux'
-        
+
         # Writing lon-lat once
         X, Y = self.grid.meshgrid
         self.v_lon[:] = X.T
@@ -237,7 +279,7 @@ class Sflux(object):
         else:
             raise Exception(f'at must be datetime or timedelta object')
 
-        self.v_time[stepi] = at.days + at.seconds/float(86400)
+        self.v_time[stepi] = at.days + at.seconds / float(86400)
         self.v_prate[stepi, :, :] = flux['prate']
 
         self.step = self.step + 1
@@ -247,17 +289,16 @@ class Sflux(object):
     def create_netcdf_rad(self):
         self.step = 0
         self.nfile = self.nfile + 1
-        self.filename = f'sflux_rad_{self.priority:1d}.{self.nfile:04d}.nc'
+        self.filename = self.filename_formatter(self)
         self.filepath = os.path.join(self.path, self.filename)
 
         # Creating the file first
         self.nc = Dataset(self.filepath, 'w', format='NETCDF4_CLASSIC')
 
-        
         # Creating the dimensions
         self.nc.createDimension(dimname='nx_grid', size=len(self.grid.x))
         self.nc.createDimension(dimname='ny_grid', size=len(self.grid.y))
-        self.nc.createDimension(dimname='time', size=None)
+        self.nc.createDimension(dimname='time', size=None) # unlimited
 
         # Creating the variables
         # Time
@@ -311,7 +352,7 @@ class Sflux(object):
         self.v_dswrf.units = 'W/m^2'
         self.v_dswrf.long_name = 'Downward Short Wave Radiation Flux'
         self.v_dswrf.standard_name = 'surface_downwelling_shortwave_flux_in_air'
-        
+
         # Writing lon-lat once
         X, Y = self.grid.meshgrid
         self.v_lon[:] = X.T
@@ -325,7 +366,7 @@ class Sflux(object):
         else:
             raise Exception(f'at must be datetime or timedelta object')
 
-        self.v_time[stepi] = at.days + at.seconds/float(86400)
+        self.v_time[stepi] = at.days + at.seconds / float(86400)
         self.v_dlwrf[stepi, :, :] = flux['dlwrf']
         self.v_dswrf[stepi, :, :] = flux['dswrf']
 
@@ -359,13 +400,15 @@ class Sflux(object):
 
     def sfluxtxt(self, dt):
         dt = dt.total_seconds()
-        max_window = self.nstep*dt/float(3600)
+        max_window = self.nstep * dt / float(3600)
         filepath = os.path.join(self.path, 'sflux_inputs.txt')
         with open(filepath, mode='w') as f:
             f.write('&sflux_inputs\n')
-            f.write('air_1_relative_weight=1.,\t!air_[12]_relative_weight set the relative ratio between datasets 1 and 2\n')
+            f.write(
+                'air_1_relative_weight=1.,\t!air_[12]_relative_weight set the relative ratio between datasets 1 and 2\n')
             f.write('air_2_relative_weight=99.,\n')
-            f.write(f'air_1_max_window_hours={max_window:.1f},\t!max. # of hours (offset from start time in each file) in each file of set 1\n')
+            f.write(
+                f'air_1_max_window_hours={max_window:.1f},\t!max. # of hours (offset from start time in each file) in each file of set 1\n')
             f.write('air_1_fail_if_missing=.true.,\t!set 1 is mandatory\n')
             f.write('air_2_fail_if_missing=.false.,\t!set 2 is optional\n')
             f.write("air_1_file='sflux_air_1',\t!file name for 1st set of 'air'\n")
@@ -379,4 +422,6 @@ class Sflux(object):
             f.write("dswrf_name='dswrf',\t!name of downward shortwave radiation variable (solar)\n")
             f.write("prate_name='prate',\t!name of precipitation rate variable\n")
             f.write('/\n')
-        
+
+
+
