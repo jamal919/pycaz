7f0e10c72be7a72531afa5eb78414cc3f02d9501
Jamal Khan <jamal919@gmail.com>
2024-09-12 23:12:53 +0200

    :memo: Adds esri_tiles in utils/plot module

--- pycaz/utils/plot.py
+++ pycaz/utils/plot.py
@@ -3,10 +3,45 @@
 
 import numpy as np
 import matplotlib as mpl
+from cartopy.io.img_tiles import GoogleTiles
+from typing import Literal, get_args
+
+ESRILayer = Literal[
+    'NatGeo_World_Map',
+    'World_Imagery',
+    'World_Physical_Map',
+    'World_Shaded_Relief',
+    'World_Street_Map',
+    'World_Terrain_Base',
+    'World_Topo_Map'
+]
+
+
+def esri_tiles(layer: ESRILayer = 'World_Imagery', cache: bool = False) -> GoogleTiles:
+    """
+    Returns an ESRI Tiles object to be used with cartopy add_image()
+
+    :param layer: Name of the ESRI tile layer, see ESRI_LAYERS list
+    :param cache: If the files to be saved in cache, default is False
+    """
+    if layer not in get_args(ESRILayer):
+        raise ValueError(f'Layer must be one of {ESRILayer}')
+
+    url = f'https://server.arcgisonline.com/ArcGIS/rest/services/{layer}/' + 'MapServer/tile/{z}/{y}/{x}.jpg'
+
+    return GoogleTiles(url=url, cache=cache)
 
 
 class MidpointNormalize(mpl.colors.Normalize):
     def __init__(self, vmin, vmax, midpoint=0, clip=False):
+        """
+        A normalize class for color scaling with a defined midpoint
+
+        :param vmin: minimum value
+        :param vmax: maximum value
+        :param midpoint: midpoint value
+        :param clip: True/False passed to Normalize class
+        """
         self.midpoint = midpoint
         mpl.colors.Normalize.__init__(self, vmin, vmax, clip)
 
