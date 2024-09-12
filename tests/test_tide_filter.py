3c38b31859c3bf48a096a0ced7d2c61ed0d0fef4
Jamal Khan <jamal919@gmail.com>
2024-09-12 23:12:03 +0200

    :memo: Updates documentation for test_tide_filter

--- tests/test_tide_filter.py
+++ tests/test_tide_filter.py
@@ -4,11 +4,14 @@
 from pycaz.tide.filters import filters
 import numpy as np
 
+
 def test_filter():
-    '''
+    """
     Sum of filter should be 1 (or very close to it)
-    '''
+
+    :return:
+    """
     for f in filters:
-        filtertotal = np.sum(filters[f]['Filter']/filters[f]['Denom'])
+        filtertotal = np.sum(filters[f]['Filter'] / filters[f]['Denom'])
         print(f, filtertotal)
         np.testing.assert_almost_equal(filtertotal, 1, decimal=5)
