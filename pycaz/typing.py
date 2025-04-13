f1a7e5494349636dece249d224e4df8696483287
Jamal Khan <jamal919@gmail.com>
2025-04-13 18:15:52 +0200

    :memo: Adds ArrayLike type

--- pycaz/typing.py
+++ pycaz/typing.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Typical types used in pycaz codes.
+
+@author: pycaz authors
+"""
+
 import os
 from typing import Union, TypeAlias
+from numpy.typing import ArrayLike
 
 PathLike: TypeAlias = str | bytes | os.PathLike
+ArrayLike: TypeAlias = ArrayLike
 
